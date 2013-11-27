4ad2c1dc0c6c1108c45fe7d138023c2e85f8d089
Thomas Rausch <thomas@rauschig.org>
2013-11-27 04:58:02 +0000

    [issue #9] introduce ArchiveStream and ArchiveEntry interfaces

---
+++ src/main/java/org/rauschig/jarchivelib/ArchiveEntry.java
@@ -0,0 +1,79 @@
+/**
+ *    Copyright 2013 Thomas Rausch
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.rauschig.jarchivelib;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+
+/**
+ * The entry of an archive.
+ * <p>
+ * The life of an {@link ArchiveEntry} is dependent on the status of the {@link ArchiveStream} it came from. Once
+ * retrieved via {@link ArchiveStream#getNextEntry()}, the entry can be used as long as the {@code ArchiveStream}
+ * remains on this entry, i.e. {@code getNextEntry()} was not called, and the stream was not since closed.
+ */
+public interface ArchiveEntry {
+
+    /**
+     * Special value indicating that the size is unknown
+     */
+    public static final long UNKNOWN_SIZE = -1;
+
+    /**
+     * The name of the entry in the archive. May refer to a file or directory or other item.
+     * 
+     * @return the name of the item
+     */
+    public abstract String getName();
+
+    /**
+     * The (uncompressed) size of the entry. May be -1 (UNKNOWN_SIZE) if the size is unknown
+     * 
+     * @return the size of the entry once uncompressed, or -1 if unknown.
+     */
+    public abstract long getSize();
+
+    /**
+     * Returns the last modified date of the entry.
+     * 
+     * @return the date the entry was last modified.
+     */
+    public abstract Date getLastModifiedDate();
+
+    /**
+     * Checks whether the given entry is a directory.
+     * 
+     * @return true if the entry refers to a directory
+     */
+    public abstract boolean isDirectory();
+
+    /**
+     * Extracts the entry to the given destination directory.
+     * <p>
+     * The destination is expected to be a writable directory.
+     * 
+     * @param destination the directory to etract the value to
+     * @return the extracted File
+     * @throws IOException propagated I/O errors by {@code java.io}
+     * @throws IllegalStateException if the entry is out of sync with the stream
+     * @throws IllegalArgumentException if the destination is not a directory, or a directory can not be created at the
+     *         given location
+     */
+    public abstract File extract(File destination) throws IOException, IllegalStateException,
+        IllegalArgumentException;
+
+}
---
+++ src/main/java/org/rauschig/jarchivelib/ArchiveStream.java
@@ -0,0 +1,76 @@
+/**
+ *    Copyright 2013 Thomas Rausch
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.rauschig.jarchivelib;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * An input stream of an archive. Can be used to retrieve each individual {@link ArchiveEntry}.
+ * <p>
+ * The {@link #getNextEntry()} method is used to reset the input stream ready for reading the data from the next entry.
+ */
+public abstract class ArchiveStream extends InputStream implements Closeable, AutoCloseable {
+
+    private ArchiveEntry currentEntry;
+
+    private boolean closed;
+
+    /**
+     * Returns the {@link ArchiveEntry} the stream currently points to.
+     * 
+     * @return the current {@link ArchiveEntry}
+     */
+    public ArchiveEntry getCurrentEntry() {
+        return currentEntry;
+    }
+
+    /**
+     * Moves the pointer of the stream to the next {@link ArchiveEntry} and returns it.
+     * 
+     * @return the next archive entry.
+     * 
+     * @throws IOException propagated I/O exception
+     */
+    public ArchiveEntry getNextEntry() throws IOException {
+        currentEntry = createNextEntry();
+        return currentEntry;
+    }
+
+    /**
+     * Abstract method to create the next {@link ArchiveEntry} for the {@link ArchiveStream} implementation.
+     * 
+     * @return the next archive entry
+     * @throws IOException propagated I/O exception
+     */
+    protected abstract ArchiveEntry createNextEntry() throws IOException;
+
+    @Override
+    public void close() throws IOException {
+        closed = true;
+    }
+
+    /**
+     * Checks whether the current stream has been closed
+     * 
+     * @return true if the stream has been closed
+     */
+    public boolean isClosed() {
+        return closed;
+    }
+
+}

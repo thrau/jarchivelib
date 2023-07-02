477b5b6e4891d59c2d791254c64f01f8953ecee1
Mohammad <mohammad.jalili.torkamani@gmail.com>
2023-07-02 18:23:53 +0330

    isDirectory method updated

--- src/main/java/org/rauschig/jarchivelib/CommonsArchiveEntry.java
+++ src/main/java/org/rauschig/jarchivelib/CommonsArchiveEntry.java
@@ -24,71 +24,71 @@ import java.util.Date;
  */
 class CommonsArchiveEntry implements ArchiveEntry {
 
-    /**
-     * The wrapped {@code ArchiveEntry} entry.
-     */
-    private org.apache.commons.compress.archivers.ArchiveEntry entry;
+	/**
+	 * The wrapped {@code ArchiveEntry} entry.
+	 */
+	private org.apache.commons.compress.archivers.ArchiveEntry entry;
 
-    /**
-     * The {@link ArchiveStream} this entry belongs to.
-     */
-    private ArchiveStream stream;
+	/**
+	 * The {@link ArchiveStream} this entry belongs to.
+	 */
+	private ArchiveStream stream;
 
-    CommonsArchiveEntry(ArchiveStream stream, org.apache.commons.compress.archivers.ArchiveEntry entry) {
-        this.stream = stream;
-        this.entry = entry;
-    }
+	CommonsArchiveEntry(ArchiveStream stream, org.apache.commons.compress.archivers.ArchiveEntry entry) {
+		this.stream = stream;
+		this.entry = entry;
+	}
 
-    @Override
-    public String getName() {
-        assertState();
-        return entry.getName();
-    }
+	@Override
+	public String getName() {
+		assertState();
+		return entry.getName();
+	}
 
-    @Override
-    public long getSize() {
-        assertState();
-        return entry.getSize();
-    }
+	@Override
+	public long getSize() {
+		assertState();
+		return entry.getSize();
+	}
 
-    @Override
-    public Date getLastModifiedDate() {
-        assertState();
-        return entry.getLastModifiedDate();
-    }
+	@Override
+	public Date getLastModifiedDate() {
+		assertState();
+		return entry.getLastModifiedDate();
+	}
 
-    @Override
-    public boolean isDirectory() {
-        assertState();
-        return entry.isDirectory();
-    }
+	@Override
+	public boolean isDirectory() {
+		// Entry should not be null
+		assert entry != null : "entry should not be null";
+		// Entry should be a directory
+		assert entry.isDirectory() : "entry should be a directory";
+		// State should be valid
+		assertState();
+		return entry.isDirectory();
+	}
 
-    @Override
-    public File extract(File destination) throws IOException, IllegalStateException, IllegalArgumentException {
-        assertState();
-        IOUtils.requireDirectory(destination);
+	@Override
+	public File extract(File destination) throws IOException, IllegalStateException, IllegalArgumentException {
+		assertState();
+		IOUtils.requireDirectory(destination);
+		File file = new File(destination, entry.getName());
+		if (entry.isDirectory()) {
+			file.mkdirs();
+		} else {
+			file.getParentFile().mkdirs();
+			IOUtils.copy(stream, file);
+		}
+		FileModeMapper.map(entry, file);
+		return file;
+	}
 
-        File file = new File(destination, entry.getName());
-
-        if (entry.isDirectory()) {
-            file.mkdirs();
-        } else {
-            file.getParentFile().mkdirs();
-            IOUtils.copy(stream, file);
-        }
-
-        FileModeMapper.map(entry, file);
-
-        return file;
-    }
-
-    private void assertState() {
-        if (stream.isClosed()) {
-            throw new IllegalStateException("Stream has already been closed");
-        }
-        if (this != stream.getCurrentEntry()) {
-            throw new IllegalStateException("Illegal stream pointer");
-        }
-    }
-
+	private void assertState() {
+		if (stream.isClosed()) {
+			throw new IllegalStateException("Stream has already been closed");
+		}
+		if (this != stream.getCurrentEntry()) {
+			throw new IllegalStateException("Illegal stream pointer");
+		}
+	}
 }
